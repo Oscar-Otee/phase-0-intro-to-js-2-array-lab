0b5d8ee0123c5ce7d0326911a9a5490f19182128
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-06-09 23:57:07 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
 
 function destructivelyAppendCat(name) {
-    const cats = ["Milo", "Otis", "Garfield"];
-    cats.push(name);
-    return cats;
+    return cats.push(name);
+    
 }
+
 
 function destructivelyPrependCat(name) {
-    const cats = ["Milo", "Otis", "Garfield"];
-    cats.unshift(name);
-    return cats;
+    return cats.unshift(name);
 }
+
 
 function destructivelyRemoveLastCat() {
-    const cats = ["Milo", "Otis", "Garfield"];
-    cats.pop();
-    return cats;
+    return cats.splice(-1);
 }
+
 
 function destructivelyRemoveFirstCat() {
-    const cats = ["Milo", "Otis", "Garfield"];
-    cats.shift();
-    return cats;
+    return cats.shift();
 }
 
+
 function appendCat(name) {
-    const cats = ["Milo", "Otis", "Garfield"];
     return [...cats, name];
 }
 
 function prependCat(name) {
-    const cats = ["Milo", "Otis", "Garfield"];
     return [name, ...cats];
 }
 
 function removeLastCat() {
-    const cats = ["Milo", "Otis", "Garfield"];
     return cats.slice(0, 2);
 }
 
 function removeFirstCat() {
-    const cats = ["Milo", "Otis", "Garfield"];
     return cats.slice(1);
 }
-console.log(destructivelyAppendCat("Ralph"));
-console.log(destructivelyPrependCat("Bob"));
-console.log(destructivelyRemoveLastCat());
-console.log(destructivelyRemoveFirstCat());
-console.log(appendCat("Broom"));
-console.log(prependCat("Arnold"));
-console.log(removeLastCat());
-console.log(removeFirstCat());
